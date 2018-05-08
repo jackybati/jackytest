aaa74cf9f36fd11bf01fe51bd3b7651fa29e70e6
huangjiayi <huangjiayi@itouchtv.cn>
2018-05-08 17:43:24 +0800

    测试路由

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    //return view('welcome');
-    Route::get('test2', 'TestController@test2');//测试
+    return view('welcome');
 });
-
+*/
 Route::group(['prefix' => 'v1', 'namespace' => 'V1'], function () {
-    Route::get('test2', 'TestController@test2');//测试
+    Route::get('/', 'TestController@test2');//测试
 });
